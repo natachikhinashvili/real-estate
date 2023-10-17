e5c57a7706728801e23cf21963043ab1c1a0f4a1
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-10-17 19:24:25 +0400

    feat: update visualising details

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # real-estate
 
 ## Visualisation of the gathered data in a creative form
-![map](./realestatemap.png)
+![map](./map.png)
 
 ## url to see dynamodb data on s3 
 http://realestatedata123.s3-website-us-east-1.amazonaws.com/data.txt
--- visualizedata.py
+++ visualizedata.py
@@ -1,8 +1,6 @@
 import boto3
-import pandas as pd
 import folium
 import requests
-import matplotlib.pyplot as plt
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table_name = 'selling-real-estate'
@@ -16,7 +14,7 @@ addresses = [item['address'] for item in property_data]
 
 m = folium.Map(location=[41.7151, 44.8271], zoom_start=12)
 
-for address in addresses:
+for index, address in enumerate(addresses):
     url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={"AIzaSyBI5ZsMHPmb2o1SESN22PJi_Dqaba6CeYY"}'
     response = requests.get(url)
     data = response.json()
@@ -27,10 +25,13 @@ for address in addresses:
         longitude = location['lng']
     else:
         print(f'Geocoding failed. Status: {data["status"]}')
-    
+
+    popup_html = f'<b>Address:</b> {address}<br><b>name:</b> {property_data[index]["name_last_name"]}<br><b>Price:</b> {property_data[index]["price"]}<br><b>real estate type:</b> {property_data[index]["real_estate_type"]}'
+    popup = folium.Popup(popup_html, max_width=300)
+
     folium.Marker(
         location=[latitude, longitude],
-        popup=address
+        popup=popup
     ).add_to(m)
 
 m.save("map.html")
