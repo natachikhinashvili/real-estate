3174aa9c8ba4160aad45ea69bfe21f06dfb8bf12
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-10-17 19:03:42 +0400

    feat: add content type while uploading txt file

--- uploadtos3.py
+++ uploadtos3.py
@@ -7,5 +7,5 @@ def uploadtos3():
     filename = "data.txt"
     mapfilename = "index.html"
 
-    s3.upload_file("data.txt", bucket, filename)
+    s3.upload_file("data.txt", bucket, filename, ExtraArgs={'ContentType': 'text/plain'})
     s3.upload_file("map.html", bucket, mapfilename,  ExtraArgs={'ContentType': 'text/html'}) 
