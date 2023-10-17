60c8d5b5f272e128056dcd62442fe83cf00eaffc
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-10-17 17:52:52 +0400

    feat: upload map html file to s3

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 
 ## url to see dynamodb data on s3 
 http://realestatedata123.s3-website-us-east-1.amazonaws.com/data.txt
+
+## map link
+http://realestatedata123.s3-website-us-east-1.amazonaws.com/index.html
--- uploadtos3.py
+++ uploadtos3.py
@@ -5,5 +5,7 @@ def uploadtos3():
     
     bucket = "realestatedata123"
     filename = "data.txt"
+    mapfilename = "index.html"
 
     s3.upload_file("data.txt", bucket, filename)
+    s3.upload_file("map.html", bucket, mapfilename)
